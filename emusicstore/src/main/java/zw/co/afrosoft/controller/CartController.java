d43017fdb826cbad54190439c72d0c1c7c48cf3b
edsonj <edsonj@Edsonj-PC.afrosoft.co.zw>
2016-09-20 12:57:15 +0200

    Saved CartControler

--- emusicstore/src/main/java/zw/co/afrosoft/controller/CartController.java
+++ emusicstore/src/main/java/zw/co/afrosoft/controller/CartController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,8 +65,30 @@ public class CartController {
 		cartDao.update(sessionId, cart);
 	}
 	
+	@RequestMapping(value="/delete/{productId}")
+	@ResponseStatus(value=HttpStatus.NO_CONTENT)
 	public void deleteItem(@PathVariable(value="productId") Integer productId, HttpServletRequest request){
 		String sessionId = request.getSession(true).getId();
+		Cart cart = cartDao.read(sessionId);
 		
+		if(cart == null){
+			cart = new Cart(sessionId);
+		}
+		
+		Product product = productDao.getProductById(productId);
+		if(product==null){
+			throw new IllegalArgumentException(new Exception());
+		}
+		
+		cart.removeCartItem(new CartItem(product)); 
+		cartDao.update(sessionId, cart);
 	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Illegal request, please verify your payload.")
+	public void handleClientError(Exception ex){}
+	
+	@ExceptionHandler(Exception.class)
+	@ResponseStatus(value=HttpStatus.INTERNAL_SERVER_ERROR, reason="Internal server error.")
+	public void handleServerError(Exception ex){}
 }
